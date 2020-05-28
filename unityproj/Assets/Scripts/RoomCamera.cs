c4671f06b00d6b029516d555664d8e5f2cff2639
Zach-Weber <zachary.weber@digipen.edu>
2020-05-28 16:43:36 -0700

    ROOM CAMERA DOESNT WORK
    I stopped trying

---
+++ unityproj/Assets/Scripts/RoomCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCamera : MonoBehaviour
+{
+
+    public GameObject virtualCamera;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Player Entered Room");
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+
+            virtualCamera.SetActive(true);
+        }
+
+
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player Exited Room");
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+
+            virtualCamera.SetActive(false);
+
+        }
+
+
+
+    }
+}
