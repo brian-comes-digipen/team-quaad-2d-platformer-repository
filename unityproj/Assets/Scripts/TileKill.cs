d49ea8099f952857df843c69414df2ec26a29ac0
Remmie <emily@isimar.com>
2020-06-01 17:00:53 -0700

    Set up enemy animations, did not set up controller

---
+++ unityproj/Assets/Scripts/TileKill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileKill : MonoBehaviour
+{
+    #region Private Methods
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController plrC = collision.gameObject.GetComponent<PlayerController>();
+            plrC.gameObject.transform.position = plrC.respawnPos;
+            print("Player collided with designated out-of-bounds tilemap, resetting player position to respawn position.");
+        }
+    }
+
+    #endregion Private Methods
+}
