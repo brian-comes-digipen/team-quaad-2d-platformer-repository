57536352e5d47955b94f6e255ddc12e24ed4fa8b
Zach-Weber <zachary.weber@digipen.edu>
2020-06-02 18:07:28 -0700

    aDdeD FiXed MenU ThAnkS

---
+++ unityproj/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public string NextScene;
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
