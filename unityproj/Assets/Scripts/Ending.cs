8053a12a78abfd0cde16a6c9b0ea3b7620e43e21
Remmie <emily@isimar.com>
2020-06-05 15:31:43 -0700

    worked with end level

---
+++ unityproj/Assets/Scripts/Ending.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    GameObject win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        win = GameObject.Find("winscreen");
+        win.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.GetComponent<Transform>().gameObject.layer == 15)
+        {
+            win.SetActive(true);
+        }
+    }
+}
