b39876eeeb8c7879971d0373400173493e6b4df6
Remmie <emily@isimar.com>
2020-06-04 22:05:39 -0700

    Made the lights dim down when you get the last statue. Right now theres a button that will dim it for you for debugging.
    Made changes to door.cs, level2, and end level but i discarded all those so zach and i wont have conflicts

---
+++ unityproj/Assets/Scripts/TorchLights.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class TorchLights : MonoBehaviour
+{
+    public static bool lightsOff = false;
+    public float lowIntensitylvl = 0;
+
+    private bool lastLightState = false;
+    private float defaultIntensitylvl = 0;
+    Light2D torchLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        torchLight = gameObject.GetComponent<Light2D>();
+        defaultIntensitylvl = torchLight.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.F))
+        {
+            lightsOff = true;
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            lightsOff = false;
+        }
+        if (lightsOff != lastLightState)
+        {
+            lastLightState = lightsOff;
+            if (lightsOff)
+                StartCoroutine(LightsOut());
+            else
+                StartCoroutine(LightsOn());
+        }
+    }
+
+    private IEnumerator LightsOut()
+    {
+        var intensity = torchLight.intensity;
+        while(intensity > lowIntensitylvl)
+        {
+            intensity -= 0.1f;
+            torchLight.intensity = intensity;
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }
+
+    private IEnumerator LightsOn()
+    {
+        var intensity = torchLight.intensity;
+        while (intensity < defaultIntensitylvl)
+        {
+            intensity += 0.1f;
+            torchLight.intensity = intensity;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+}
+//add to door in the doorvalue if statement incase it gets removed:                 TorchLights.lightsOff = true;
